09e60699a3735c321f9fb567bddafc7b20fc2b86
dongjin <526631388@qq.com>
2019-11-26 17:42:12 +0800

    init

---
+++ src/main/java/cn/fanyu/clock/ClockApplication.java
@@ -0,0 +1,13 @@
+package cn.fanyu.clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cn/fanyu/clock/dto/ActivityInDto.java
@@ -0,0 +1,20 @@
+package cn.fanyu.clock.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @version V3.0
+ * @Title: ActivityInDto
+ * @Company: 成都影达科技有限公司
+ * @Description: 获取活动信息
+ * @author: 东进
+ * @date 2019/11/26 下午4:45
+ */
+@Getter
+@Setter
+public class ActivityInDto {
+    private String name;
+    private String endTime;
+}
